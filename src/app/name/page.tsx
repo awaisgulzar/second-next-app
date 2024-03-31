7c0775a804da6c04a3f3fbd8f91f69bcd9b7f8ad
“[awaisgulzar]” <[mawaisjani78563@gmail.com]>
2024-03-31 09:18:03 +0500

    initial commit

--- src/app/name/page.tsx
+++ src/app/name/page.tsx
@@ -5,6 +5,7 @@ export default function Name(){
     return (
         <main>
             <button onClick={()=>router.back()}>Go to back</button>
+            <h1>Hello, I am Muhammad Awais and this is my first Next.js project.</h1>
         </main>
     )
 }
